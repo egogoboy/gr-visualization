a1b651c7b9918acc6712944094ac8a42ca25f337
egogoboy <egor2005sudakov@gmail.com>
2025-11-14 10:00:55 +0300

    Changed install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apt update
-apt install qtbase5-dev libqt5svg-dev libqt5core5a libqt5widgets5 python3 python3-numpy python3-nmea2
+apt install qtbase5-dev libqt5svg5-dev libqt5core5a libqt5widgets5 python3 python3-numpy python3-nmea2
 cd external/QFlightinstruments/qfi
 qmake qfi.pro
 make
@@ -12,7 +12,7 @@ make install
 cd ../../../
 mkdir build
 cd build
-cmake ..
+cmake -DCMAKE_INSTALL_LIBDIR=lib ..
 make
 make install
 
